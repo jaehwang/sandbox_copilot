84ca2f01e912837a90a1a0a7287766ec98632332
Jaehwang Kim <kim.jaehwang@gmail.com>
2024-08-31 22:27:04 +0900

    Hide Stack APIs

--- queue.c
+++ queue.c
@@ -3,7 +3,7 @@
 #include <string.h> // Add this line to include the <string.h> header file
 #include "queue.h"
 
-void stack_push(Stack* stack, int* x) {
+static void stack_push(Stack* stack, int* x) {
     if (stack->top == stack->capacity) {
         stack->capacity *= 2;
         stack->data = (int*)realloc(stack->data, stack->capacity * sizeof(int));
@@ -11,11 +11,7 @@ void stack_push(Stack* stack, int* x) {
     stack->data[stack->top++] = *x;
 }
 
-// stack_pop 함수를 구현해 주세요.
-// 스택에서 가장 위에 있는 요소를 제거하고 반환합니다.
-// 반환 값은 데이터에 대한 포인터입니다.
-// 데이터가 없으면 NULL을 반환합니다.
-int* stack_pop(Stack* stack) {
+static int* stack_pop(Stack* stack) {
     if (stack->top == 0) {
         return NULL;
     }
@@ -23,23 +19,23 @@ int* stack_pop(Stack* stack) {
     return stack->data + stack->top;
 }
 
-int* stack_peek(Stack* stack) {
+static int* stack_peek(Stack* stack) {
     if (stack->top == 0) {
         return NULL;
     }
     return stack->data + (stack->top - 1);
 }
 
-int stack_empty(Stack* stack) {
+static int stack_empty(Stack* stack) {
     return stack->top == 0;
 }
 
-void stack_destroy(Stack* stack) {
+static void stack_destroy(Stack* stack) {
     free(stack->data);
     free(stack);
 }
 
-Stack* stack_create() {
+static Stack* stack_create() {
     Stack* stack = (Stack*)malloc(sizeof(Stack));
     stack->data = (int*)malloc(sizeof(int));
     stack->capacity = 1;
--- queue.h
+++ queue.h
@@ -15,13 +15,6 @@ typedef struct {
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-    void stack_push(Stack* stack, int* x);
-    int* stack_pop(Stack* stack);
-    int* stack_peek(Stack* stack);
-    int stack_empty(Stack* stack);
-    void stack_destroy(Stack* stack);
-    Stack* stack_create();
     void enqueue(Queue* queue, int* x);
     int* dequeue(Queue* queue);
     int* peek(Queue* queue);
